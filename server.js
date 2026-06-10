7eb5b3bf458383441d296434b3f47d39acd6ecf8
hasanimanov <44087640+HasanImanov@users.noreply.github.com>
2026-06-10 13:55:41 +0400

    Fix: xett CSS elave edildi

--- server.js
+++ server.js
@@ -271,6 +271,7 @@ app.post('/api/arayish-pdf', async (req, res) => {
       + '.arayish{text-align:center;font-size:12pt;font-weight:bold;margin:4mm 0 3mm 0;}'
       + '.metn{font-size:12pt;text-align:justify;line-height:1.3;margin-bottom:0;}'
       + '.indent{text-indent:12.5mm;}'
+      + '.xett{border:none;border-top:1.5px solid #000;margin:0 0 2mm 0;}'
       + '.imza{display:flex;justify-content:space-between;font-size:12pt;font-weight:bold;margin-top:10mm;}'
       + '</style></head><body>'
       + '<img class="header-img" src="data:image/png;base64,'+GERB+'"/>'
