6f786e8b7db8922ae2c2203b703b7889bc1728a5
hasanimanov <44087640+HasanImanov@users.noreply.github.com>
2026-06-11 11:00:02 +0400

    fix: imza padding-left 12.5mm - Direktor muavini indent duzeldildi

--- server.js
+++ server.js
@@ -272,7 +272,7 @@ app.post('/api/arayish-pdf', async (req, res) => {
       + '.metn{font-size:12pt;text-align:justify;line-height:1.3;margin-bottom:0;}'
       + '.indent{text-indent:12.5mm;}'
       + '.xett{border:none;border-top:1px solid #000;position:relative;left:-14.1mm;width:188mm;margin-top:1mm;margin-bottom:1mm;}'
-      + '.imza{display:flex;justify-content:space-between;font-size:12pt;font-weight:bold;margin-top:10mm;}'
+      + '.imza{display:flex;justify-content:space-between;font-size:12pt;font-weight:bold;margin-top:10mm;padding-left:12.5mm;}'
       + '</style></head><body>'
       + '<img class="header-img" src="data:image/png;base64,'+GERB+'"/>'
       + '<span class="bosh">&nbsp;</span>'
